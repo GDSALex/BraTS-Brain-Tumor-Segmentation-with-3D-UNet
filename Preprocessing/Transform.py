a60e609a6b3526287d44d7fe47e00d9590b05e9e
GDSALex <168967131+GDSALex@users.noreply.github.com>
2024-08-24 12:47:18 +0200

    Create Transform.py
    Transformation pipeline using TorchIO predefined transformatiops to guarantee a good training.

---
+++ Preprocessing/Transform.py
@@ -0,0 +1,36 @@
+
+
+import torchio as tio
+import torch
+
+landmarks_dict = {}
+volume_types = ["t1c", "t1n", "t2f", "t2w"]
+
+for volume in volume_types:
+    landmarks_dict[volume] = torch.load(f'{volume}_landmarks.pt')
+
+
+# Define transformation pipeline
+def transform():
+    include = ['t1c', 't1n', 't2f', 't2w']
+    transform = tio.Compose([
+        tio.ToCanonical(include=include),
+        tio.CropOrPad((148, 148, 148), include=include + ['seg']),
+        tio.RandomFlip(axes=['LR', 'AP', 'IS'], p=0.2, include=include + ['seg']),
+        tio.HistogramStandardization(landmarks_dict, include=include), #Error in generalization over non-standarized images
+        tio.ZNormalization(include=include),  
+        tio.OneOf({
+            tio.RandomAffine(include=include + ['seg']): 0.2,
+            tio.RandomElasticDeformation(include=include + ['seg']): 0.2,
+        }),
+        tio.OneOf({
+            tio.RandomNoise(include=include): 0.4,
+            tio.RandomSpike(include=include):0.1,
+        }),
+        tio.RandomBiasField(p=0.2, include=include),
+        tio.RandomGamma(log_gamma=(-0.3, 0.3), p=0.2, include=include),
+        
+    ])
+    return transform
+
+
